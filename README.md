204ed4ca9ec4aba3df24279c98151c3022aaf20d
Rahul Gunasekaran <rahulgu@synopsys.com>
2020-10-01 16:26:56 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# intelligent-security-scan
+# Synopsys Intelligent Security Scan
+
+## Overview
+
+The Synopsys Intelligent Security Scan Action helps selectively perform SAST and SCA scans, triggered during a variety of GitHub Platform events, such as push or pull request. The Synopsys Intelligent Security Scan Action allows your projects to run the only required type of security scans, optimizing the time taken by security testing and provide a quicker feedback on scan results.
+
+## Prerequisites
+
+Intelligent Scan server-side components – IQ and Workflow – must be setup on an instance, accessible to GitHub actions.
+
+## Example YAML config
+
+```yaml
+name: "Synopsys Intelligent Security Scan"
+
+on:
+  push:
+    branches: [master]
+  pull_request:
+    branches: [master]
+
+jobs:
+  security:
+    name: security scans
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Checkout repository
+      uses: actions/checkout@v2
+
+    # If this run was triggered by a pull request event, then checkout
+    # the head of the pull request instead of the merge commit.
+    - run: git checkout HEAD^2
+      if: ${{ github.event_name == 'pull_request' }}
+
+    - name: Synopsys Intelligent Security Scan
+      id: prescription
+      uses: sig-devsecops/synopsys-intelligent-scan@v1.1
+      with:
+        ioServerHost: "${{ secrets.IO_SERVER_HOST}}"
+        ioServerToken: "${{ secrets.IO_SERVER_TOKEN}}"
+        workflowServerToken: "${{ secrets.WORKFLOW_SERVER_TOKEN}}"
+        additionalWorkflowArgs: ""
+        stage: "IO"
+
+    # Scan to be performed, please note the ID in previous step was
+    # set to prescription in order for this to work
+    - name: Scan Triggers
+      run: |
+        echo "SAST: ${{steps.prescription.outputs.sastScan}}"
+        echo "SCA: ${{ steps.prescription.outputs.scaScan}}"
+        echo "DAST: ${{steps.prescription.outputs.dastScan}}"
+
+    - name: Synopsys Intelligent Security Scan
+      uses: sig-devsecops/synopsys-intelligent-scan@v1.1
+      with:
+        ioServerHost: "${{ secrets.IO_SERVER_HOST}}"
+        ioServerToken: "${{ secrets.IO_SERVER_TOKEN}}"
+        workflowServerToken: "${{ secrets.WORKFLOW_SERVER_TOKEN}}"
+        additionalWorkflowArgs: "--slack.token=${{secrets.SLACK_TOKEN}} --IS_SAST_ENABLED=${{steps.prescription.outputs.sastScan}}"
+        stage: "WORKFLOW"
+```
---
+++ index.js
@@ -0,0 +1,70 @@
+const core = require('@actions/core');
+const shell = require('shelljs');
+const fs = require('fs');
+
+try {
+	const ioServerHost = core.getInput('ioServerHost');
+	const ioServerPort = core.getInput('ioServerPort');
+	const ioServerToken = core.getInput('ioServerToken');
+	var workflowServerHost = core.getInput('workflowServerHost');
+	const workflowServerPort = core.getInput('workflowServerPort');
+	const workflowServerToken = core.getInput('workflowServerToken');
+	const workflowVersion = core.getInput('workflowVersion');
+	const applicationManifest = core.getInput('applicationManifest');
+	const ioManifest = core.getInput('ioManifest');
+	const workflowManifest = core.getInput('workflowManifest');
+	const additionalWorkflowArgs = core.getInput('additionalWorkflowArgs')
+	const stage = core.getInput('stage')
+	var rcode = -1
+
+	if(workflowServerHost === "ioServerHost") {
+		workflowServerHost = ioServerHost
+	}
+
+	// Irrespective of Machine this should be invoked
+	if(stage.toUpperCase() === "IO") {
+		console.log("Triggering prescription")
+		shell.exec(`wget https://sigdevsecops.blob.core.windows.net/intelligence-orchestration/${workflowVersion}/prescription.sh`)
+		shell.exec(`chmod +x prescription.sh`)
+		shell.exec(`sed -i -e 's/\r$//' prescription.sh`)
+		rcode = shell.exec(`./prescription.sh --IO.url=${ioServerHost}:${ioServerPort} --IO.token=${ioServerToken} --app.manifest.path=${applicationManifest} --sec.manifest.path=${ioManifest} --stage=${stage}`).code;
+
+		if (rcode != 0){
+			core.error(`Error: Execution failed and returncode is ${rcode}`);
+			core.setFailed(error.message);
+		}
+	}
+	else if (stage.toUpperCase() === "WORKFLOW")  {
+		console.log("Adding scan tool parameters")
+		// file doesn't exist
+		if (!fs.existsSync("prescription.sh")) {
+			shell.exec(`wget https://sigdevsecops.blob.core.windows.net/intelligence-orchestration/${workflowVersion}/prescription.sh`)
+			shell.exec(`chmod +x prescription.sh`)
+			shell.exec(`sed -i -e 's/\r$//' prescription.sh`)
+		}
+		var wffilecode = shell.exec(`./prescription.sh --stage=${stage} --workflow.template=${workflowManifest} ${additionalWorkflowArgs}`).code;
+		if (wffilecode == 0) {
+			console.log("Workflow file generated successfullly....Calling WorkFlow Engine")
+			shell.exec(`wget https://sigdevsecops.blob.core.windows.net/intelligence-orchestration/${workflowVersion}/WorkflowClient.jar`)
+			shell.exec(`chmod +x WorkflowClient.jar`)
+			var wfclientcode = shell.exec(`java -jar WorkflowClient.jar --workflowengine.url="${workflowServerHost}:${workflowServerPort}" --workflowengine.token="${workflowServerToken}" --app.manifest.path=${applicationManifest} --sec.manifest.path=synopsys-io-workflow.yml`).code;
+			if (wfclientcode != 0) {
+				core.error(`Error: Workflow failed and returncode is ${wfclientcode}`);
+				core.setFailed(error.message);
+			}
+		}
+		else {
+			core.error(`Error: Workflow file generation failed and returncode is ${wffilecode}`);
+			core.setFailed(error.message);
+		}
+	}
+	else {
+		core.error(`Error: Invalid stage given as input`);
+		core.setFailed(error.message);
+	}
+}
+
+catch (error) {
+	core.setFailed(error.message);
+}
+
